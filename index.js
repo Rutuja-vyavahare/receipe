e05b69ba382d66e26196375e8dc20b6e8c609a98
Rutuja-vyavahare <rutujavyavahare15@gmail.com>
2025-03-27 23:18:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
-const router = express.Router();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-app.use(express.json());
-app.use(cors());
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const config = require("./db/config");
-const Home = require("./controllers/controller");
-const LoginRoute = require("./routes/LoginRoute");
-const RegisterRoute = require("./routes/RegisterRoute");
-const verifyToken = require("./Middleware/middleware");
-const RecipeRoute = require("./routes/RecipeRoute");
-const ForgotPassword = require("./routes/forgotPassword");
-
-app.use("/auth", LoginRoute);
-app.use("/auth", RegisterRoute);
-app.use("/auth", RecipeRoute);
-app.use("/auth", router);
-app.use("/auth", ForgotPassword);
-
-router.get("/", verifyToken, Home.Home);
-
-module.exports = router;
-
-if (config) {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server Started on port ${process.env.PORT}`);
-  });
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
